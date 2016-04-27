00505bbb2f1de445b9b8f8ac2d120577e5e6b5f5
Arnout Engelen <arnouten@bzzt.net>
2016-04-27 12:31:19 +0200

    Naive implementation - works worse than chance at this point :)

--- src/main/scala/someskill/Engine.scala
+++ src/main/scala/someskill/Engine.scala
@@ -11,16 +11,36 @@ object Engine {
 class Engine {
   import Engine._
 
-  case class Skill(buckets: Array[Double])
+  case class Skill(buckets: Array[Double]) {
+    lazy val level = buckets.zipWithIndex.map { case (value, idx) => (idx - (numberOfBuckets / 2)) * value }.sum
+  }
 
-  private val defaultSkill: Skill = Skill(Array.fill(100)(0.0))
-  private var distributions: Map[Player, Skill] = Map.empty.withDefaultValue(defaultSkill)
+  private val numberOfBuckets = 100
+  private[someskill] val defaultSkill: Skill = Skill(Array.fill(numberOfBuckets)(0.1))
+  private var distribution: Map[Player, Skill] = Map.empty.withDefaultValue(defaultSkill)
+
+  def calculateDistributions(winner: Array[Double], loser: Array[Double]) = {
+    val newWinner = winner.zipWithIndex.map {
+      case (oldValue, idx) => Range(0, idx).map(loser(_) * oldValue).sum
+    }
+    val newLoser = loser.zipWithIndex.map {
+      case (oldValue, idx) => Range(idx, numberOfBuckets - 1).map(winner(_) * oldValue).sum
+    }
+    // I guess we'll have to normalize the new distributions?
+    (newWinner, newLoser)
+  }
 
   def learn(game: Game) = {
-    // TODO implement actually learning :)
+    val (newWinnerDistribution, newLoserDistribution) =
+      calculateDistributions(distribution(game.winner).buckets, distribution(game.loser).buckets)
+
+    distribution = distribution +
+      (game.winner -> Skill(newWinnerDistribution))
+      (game.loser -> Skill(newLoserDistribution))
   }
 
   def predictWinner(one: Player, two: Player): Player =
-    if (Random.nextBoolean()) one
+    if (distribution(one).level > distribution(two).level) one
+    else if (distribution(one).level == distribution(two).level && Random.nextBoolean()) one
     else two
 }
---
+++ src/test/scala/TestDistributionUpdate.scala
@@ -0,0 +1,17 @@
+package someskill
+
+import org.scalatest._
+
+class TestDistributionUpdate extends WordSpec with Matchers {
+  "The distribution update function" should {
+    "update distributions correctly when a match is won" in {
+      val engine = new someskill.Engine()
+      import engine._
+
+      val (winner, loser) = calculateDistributions(defaultSkill.buckets, defaultSkill.buckets)
+      println(s"New winner distribution: ${winner.mkString(",")}")
+      println(s"New loser distribution: ${loser.mkString(",")}")
+      Skill(winner).level should be > Skill(loser).level
+    }
+  }
+}

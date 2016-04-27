e0d17aa6a38b03413231bcbab628a5304a6de748
Arnout Engelen <arnouten@bzzt.net>
2016-04-27 11:26:48 +0200

    Reading 1968 match data and training the engine

--- src/main/scala/someskill/Tennis.scala
+++ src/main/scala/someskill/Tennis.scala
@@ -1,5 +1,16 @@
 package someskill
 
+import scala.io._
+
 object Tennis extends App {
-  println("Hello, world")
+  val engine = new Engine()
+
+  Source.fromURL(Tennis.getClass.getResource("/atp/atp_matches_1968.csv"))(Codec.UTF8)
+    .getLines()
+    .drop(1)
+    .map(line => {
+      val columns = line.split(",")
+      Engine.Game(winner = columns(10), loser = columns(20))
+    })
+    .foreach(engine.learn(_))
 }
